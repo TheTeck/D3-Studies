df1c18f82733c626614ad95bc0839616163be915
John Teckemeyer <johnteckemeyer@gmail.com>
2021-11-14 19:36:55 -0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { select, json } from 'd3';
+
 import './App.css';
 
 function App() {
+
+  const data = [
+    { "country": "China", "population": 1415046},
+    { "country": "India", "population": 1354052},
+    { "country": "United States", "population": 326767},
+    { "country": "Indonesia", "population": 266795},
+    { "country": "Brazil", "population": 210868},
+    { "country": "Pakistan", "population": 200814},
+    { "country": "Nigeria", "population": 195875},
+    { "country": "Bangladesh", "population": 166368},
+    { "country": "Russia", "population": 143965},
+    { "country": "Mexico", "population": 130759}
+  ]
+
+  const svg = select('svg');
+
+  const height = +svg.attr('height');
+  const width = +svg.attr('width');
+
+  data.forEach(d => {
+    d.population = d.population * 1000;
+  })
+
+  console.log(data)
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {width} X {height}
     </div>
   );
 }
