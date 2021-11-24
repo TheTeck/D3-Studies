70d07ec9674310924beae457ab6308ea153b6da8
John Teckemeyer <johnteckemeyer@gmail.com>
2021-11-23 21:39:04 -0800

    Go through process of creating basic line graph using an array for data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
 import './App.css';
-import { message } from './message';
-
-const height = 500;
-const width = 960;
-const csvUrl = 'https://gist.githubusercontent.com/JosephStalnaker/539ae314a112da952df3e5b9af7eef48/raw/c0457df5300920603141bce472b4996a8b3d2a2a/cssNamedColors.csv';
 
 function App() {
-  
-  const [data, setData] = useState(null);
+  const [data] = useState([25, 50, 35, 60, 15, 94, 10])
+  const svgRef = useRef();
 
   useEffect(() => {
-    d3.csv(csvUrl).then(setData);
-  }, [])
+    // setting up svg
+    // setting the scaling
+    // set the axis
+    // setting up the data for the svg
+    const w = 400;
+    const h = 100;
+    const svg = d3.select(svgRef.current)
+      .attr('width', w)
+      .attr('height', h)
+      .style('background', 'linear-gradient(white, grey)')
+      .style('margin-top', 50)
+      .style('overflow', 'visible')
+
+    const xScale = d3.scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, w])
+    
+    const yScale = d3.scaleLinear()
+      .domain([0, h])
+      .range([h, 0])
+
+    const generateScaledLine = d3.line()
+      .x((d, i) => xScale(i))
+      .y(yScale)
+      .curve(d3.curveCardinal)
+    
+    svg.selectAll('.line')
+      .data([data])
+      .join('path')
+        .attr('d', d => generateScaledLine(d))
+        .attr('fill', 'none')
+        .attr('stroke', 'orange')
+        .attr('stroke-width', 5)
+
+    const xAxis = d3.axisBottom(xScale)
+      .ticks(data.length)
+      .tickFormat(i => i + 1)
+    
+    const yAxis = d3.axisLeft(yScale)
+      .ticks(5)
+    
+    svg.append('g')
+      .call(xAxis)
+      .attr('transform', `translate(0, ${h})`)
+
+    svg.append('g')
+      .call(yAxis)
+  }, [data])
 
   return (
     <div className="App">
-      Data is { data ? message(data) : 'loading' }
+      <svg ref={svgRef}></svg>
     </div>
   );
 }
