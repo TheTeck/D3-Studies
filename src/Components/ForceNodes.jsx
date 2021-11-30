e7066c4c96b9c622f3dc0b05e17d6d713490989a
John Teckemeyer <johnteckemeyer@gmail.com>
2021-11-29 17:38:27 -0800

    Explore the capabilities of using nodes and links for graphs

--- src/Components/ForceNodes.jsx
+++ src/Components/ForceNodes.jsx
@@ -6,20 +6,34 @@ const height = 400;
 
 const graph = {
     nodes: [
-        { name: 'Alice' },
-        { name: 'Bob' },
-        { name: 'Chen' },
-        { name: 'Dan' },
-        { name: 'Ethan' },
-        { name: 'Frank' },
-        { name: 'Hans' }
+        { name: 'A' },
+        { name: 'B' },
+        { name: 'C' },
+        { name: 'D' },
+        { name: 'E' },
+        { name: 'F' },
+        { name: 'G' },
+        { name: 'H' },
+        { name: 'I' },
+        { name: 'J' }
     ],
     links: [
-        { source: 'Alice', target: 'Bob' },
-        { source: 'Chen', target: 'Bob' },
-        { source: 'Dan', target: 'Chen' },
-        { source: 'Hans', target: 'Frank' },
-        { source: 'Hans', target: 'Ethan'}
+        { source: 'A', target: 'B' },
+        { source: 'B', target: 'C' },
+        { source: 'C', target: 'D' },
+        { source: 'D', target: 'A' },
+        { source: 'E', target: 'A' },
+        { source: 'F', target: 'B' },
+        { source: 'G', target: 'C' },
+        { source: 'H', target: 'D' },
+        { source: 'E', target: 'F' },
+        { source: 'G', target: 'H' },
+        { source: 'I', target: 'A' },
+        { source: 'I', target: 'D' },
+        { source: 'I', target: 'E' },
+        { source: 'J', target: 'B' },
+        { source: 'J', target: 'C' },
+        { source: 'J', target: 'G' }
     ]
 }
 
@@ -36,7 +50,7 @@ export default function ForceNodes (props) {
         let simulation = d3
             .forceSimulation(data.nodes)
             .force("link", d3.forceLink(data.links).id(d => d.name))
-            .force("charge", d3.forceManyBody().strength(-30))
+            .force("charge", d3.forceManyBody().strength(-50))
             .force("center", d3.forceCenter(width / 2, height / 2))
             .on("tick", ticked);
         
