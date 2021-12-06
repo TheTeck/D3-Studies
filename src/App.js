e662bfc39dd2ea7f97f12e8b6faf3934f20d61ef
John Teckemeyer <johnteckemeyer@gmail.com>
2021-12-06 14:25:09 -0800

    Create a bar chart with multiple sets of data displayed next to each other at each tick of the x-axis

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LineGraph from './Components/LineGraph';
 import ForceNodes from './Components/ForceNodes';
 import BubbleChart from './Components/BubbleChart';
 import HistogramChart from './Components/HistogramChart';
+import MultiBarChart from './Components/MultiBarChart';
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <LineGraph />
       <HistogramChart />
+      <MultiBarChart />
       <ForceNodes />
       <BubbleChart />
     </div>
