ef8dcb55bbaf9fa76432861907e5dbae87ab2bfb
John Teckemeyer <johnteckemeyer@gmail.com>
2021-12-07 20:16:11 -0800

    Refactor histogram to use two work with two arrays side by side with one selection

--- src/Components/MultiBarChart.jsx
+++ src/Components/MultiBarChart.jsx
@@ -10,11 +10,11 @@ const width = 1000;
 const height = 400;
 const marginX = 50;
 const marginY = 30;
+const groupLength = temps[0].length;
 
 export default function MultiBarChart (props) {
 
-    const [data, setData] = useState(temps[0]);
-    const [data2, setData2] = useState(temps[1]);
+    const [data] = useState([...temps[0], ...temps[1]]);
 
     const svgRef = useRef();
 
@@ -24,9 +24,9 @@ export default function MultiBarChart (props) {
             .attr('height', height + marginY + marginY)
 
         const x = d3.scaleBand()
-            .domain(d3.range(data.length))
+            .domain(d3.range(groupLength))
             .range([marginX, width + marginX])
-            .padding(0.2)
+            .padding(0.1)
 
         const y = d3.scaleLinear()
             .domain([20, 80])
@@ -34,94 +34,25 @@ export default function MultiBarChart (props) {
 
         svg
             .append('g')
-            .attr('fill', 'royalblue')
             .selectAll('rect')
             .data(data)
-            .join('rect')
-                .attr('x', (d, i) => x(i))
-                .attr('y', d => y(d))
-                .attr('height', d => y(20) - y(d))
-                .attr('width', x.bandwidth()/2)
-                .on('mouseover', showValue)
-                .on('mouseleave', hideValue)
-        
-        svg
-            .append('g')
-            .attr('fill', 'blue')
-            .selectAll('rect')
-            .data(data2)
-            .join('rect')
-                .attr('x', (d, i) => x(i) + x.bandwidth()/2)
-                .attr('y', d => y(d))
-                .attr('height', d => y(20) - y(d))
-                .attr('width', x.bandwidth()/2)
-                .on('mouseover', showValue)
-                .on('mouseleave', hideValue)
-
-        const generateScaledLine = d3.line()
-            .x((d, i) => x(i))
-            .y(y)
-            .curve(d3.curveCardinal)
-              
-        svg.selectAll('.line')
-            .data([data])
-            .join('path')
-                .attr('d', d => generateScaledLine(d))
-                .attr('fill', 'none')
-                .attr('stroke', 'orange')
-                .attr('stroke-width', 3)
-                .attr('transform', `translate(${x.bandwidth()/4}, 0)`)
-        
-        svg.selectAll('.line')
-            .data([data2])
-            .join('path')
-                .attr('d', d => generateScaledLine(d))
-                .attr('fill', 'none')
-                .attr('stroke', 'pink')
-                .attr('stroke-width', 3)
-                .attr('transform', `translate(${(x.bandwidth()/4) * 3}, 0)`)
-
-        function showValue () {
-            this.parentNode.appendChild(this);
-
-            d3.select(this)
-                .transition()
-                    .duration(300)
-                    .attr('transform', `translate(-100, 0)`)
-                    .attr('width', 200)
-                    .attr('height', 200)
-                    .attr('stroke', 'purple')
-                    .attr('stroke-width', 4)
-                    .attr('zindex', 3)
-        }
-
-        function hideValue () {
-            d3.select(this)
-                .transition()
-                    .duration(300)
-                    .attr('transform', 'translate(0, 0)')
+                .join('rect')
+                    .attr('fill', (d, i) => i < groupLength ? "royalBlue": "blue")
+                    .attr('x', (d, i) => i < groupLength ? x(i % groupLength) + x.bandwidth() / 2 : x(i % groupLength))
+                    .attr('y', d => y(d))
+                    .attr('height', d => y(20) - y(d))
                     .attr('width', x.bandwidth()/2)
-                    .attr('height', d =>y(20) - y(d))
-                    .attr('stroke', 'none')
-        }
 
-        function xAxis (g) {
-            g
-                .attr('transform', `translate(0, ${height})`)
-                .call(d3.axisBottom(x).tickFormat(i => i))
-                .attr('font-size', '20px')
-        }
+        svg.append('g')
+            .attr('transform', `translate(0, ${height})`)
+            .call(d3.axisBottom(x).tickFormat(i => i))
+            .attr('font-size', '20px')
 
-        function yAxis (g) {
-            g
-                .attr('transform', `translate(${marginX}, 0)`)
-                .call(d3.axisLeft(y).ticks(null, data.format))
-                .attr('font-size', '20px')
-        }
+        svg.append('g')
+            .attr('transform', `translate(${marginX}, 0)`)
+            .call(d3.axisLeft(y).ticks(null, data.format))
+            .attr('font-size', '20px')
         
-
-        svg.append('g').call(xAxis);
-        svg.append('g').call(yAxis);
         svg.node();
     }, [data]);
 
